68b4553fed21532ca9994b656e3ffe711a37e58c
Corban <sonnemanc@gmail.com>
2022-02-16 14:07:47 -0800

    commit tree 81cb26878f0c1eeb87e6a5c4a9e463d48d4e9a60

--- index.js
+++ index.js
@@ -9,6 +9,59 @@
  for you to use if you need it!
  */
 
+let createEmployeeRecord = function(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(arrs) {
+  return arrs.map(function(arr) {
+      return createEmployeeRecord(arr)
+  })
+}
+
+let createTimeInEvent = function (dateStamp) {
+    let [date, hour] = dateStamp.split( ' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(date) {
+  let timeIn = this.timeInEvents.find(function(a) {
+      return a.date === date
+  })
+  let timeOut = this.timeOutEvents.find(function(b) {
+      return b.date === date
+  })
+  return (timeOut.hour - timeIn.hour) / 100
+}
+
+let wagesEarnedOnDate = function(date) {
+    let rawWage = hoursWorkedOnDate.call(this, date)
+      * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +72,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords) {
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(rec){
+      return rec.firstName === firstName
+    })
 }
